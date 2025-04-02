cfd9e10e2efb74398cee6cf3233d04bfd562d877
Salma Elakhal <salma.elakhal.pro@gmail.com>
2025-04-03 01:28:42 +0200

    completed geolocation

--- front-end/src/components/SearchBar/SearchBar.jsx
+++ front-end/src/components/SearchBar/SearchBar.jsx
@@ -1,88 +1,93 @@
-import { Button, Form } from "react-bootstrap";
-import styles from "./SearchBar.module.scss";
-import { Autocomplete, Switch, TextField } from "@mui/material";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { resetData, setData } from "../../features/weather/WeatherSlice";
+import styles from './SearchBar.module.scss'
+import {Autocomplete, Button, TextField} from "@mui/material";
+import {useEffect, useState} from "react";
+import {useDispatch} from "react-redux";
+import {resetData, setData} from "../../features/weather/WeatherSlice";
+import PositionSvg from "../Svgs/PositionSvg";
 
 export const SearchBar = () => {
-  const [cities, setCities] = useState([]);
-  // const [geoLocation, setGeoLocation] = useState(undefined);
-  // const [isCurrentLocation, setIsCurrentLocation] = useState(false);
-  const dispatch = useDispatch();
-  const [unity, setUnity] = useState('metric')
+    const GEO_API_KEY = process.env.REACT_APP_GEO_API_KEY
+    const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API
 
 
-  const GEO_API_KEY = process.env.REACT_APP_GEO_API_KEY;
-  const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API;
-
-  const handleInputChange = (e) => {
-    const { value } = e.currentTarget;
-    fetch(
-      `https://api.geoapify.com/v1/geocode/autocomplete?text=${value}&type=city&format=json&apiKey=${GEO_API_KEY}`
-    )
-      .then((response) => response.json())
-      .then((json) =>
-        setCities(
-          json.results?.map((data) => {
-            const { lat, lon, city, country, formatted } = data;
-            return { lat, lon, city, country, formatted };
-          })
-        )
-      );
-  };
+    const dispatch = useDispatch()
+    const [cities, setCities] = useState([])
+    const [unity] = useState('metric')
 
 
-  const handleAutocompleteSelect = (e, value) => {
-    if (value !== null) {
-        const {lon, lat} = value
-        fetch(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&units=${unity}&lon=${lon}&appid=${WEATHER_API_KEY}`
-        )
-          .then((response) => response.json())
-          .then((json) => {
-            const { clouds, main, name, sys, weather, wind } = json;
-            dispatch(setData({ clouds, main, name, sys, weather, wind }));
-          });
-        // setIsCurrentLocation(false)
-        // setGeoLocation({
-        //     lon,
-        //     lat,
-        // })
+    const [geoLocation, setGeoLocation] = useState(undefined)
+    const [isCurrentLocation, setIsCurrentLocation] = useState(false)
 
-    } else {
-        dispatch(resetData())
+
+    useEffect(() => {
+      getGeoLocation()
+  }, []);
+  useEffect(() => {
+      getData()
+  }, [geoLocation]);
+
+
+
+    const getGeoLocation = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            setIsCurrentLocation(true)
+            setGeoLocation({
+                lon: position.coords.longitude,
+                lat: position.coords.latitude,
+            })
+        })
     }
 
+
+    const handleInputChange = (e) => {
+        const {value} = e.currentTarget
+        fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${value}&type=city&format=json&apiKey=${GEO_API_KEY}`)
+            .then(response => response.json())
+            .then(json => setCities(json.results?.map(data => {
+                const {lat, lon, city, country, formatted} = data
+                return {lat, lon, city, country, formatted}
+            })))
+    }
+    const getData = () => {
+        if (geoLocation) {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geoLocation.lat}&units=${unity}&lon=${geoLocation.lon}&appid=${WEATHER_API_KEY}`)
+                .then(response => response.json())
+                .then(json => {
+                    const {clouds, main, name, sys, weather, wind} = json
+                    dispatch(setData({clouds, main, name, sys, weather, wind}))
+                })
+        }
+    }
+    const handleAutocompleteSelect = (e, value) => {
+        if (value !== null) {
+            const {lon, lat} = value
+            setIsCurrentLocation(false)
+            setGeoLocation({
+                lon,
+                lat,
+            })
+
+        } else {
+            dispatch(resetData())
+        }
+
+    }
+    return (
+        <>
+            <div
+                className={styles.searchContainer}>
+                <Autocomplete className={styles.searchInput}
+                              clearOnBlur={false}
+                              onChange={handleAutocompleteSelect}
+                              getOptionLabel={(option) => option.formatted}
+                              renderInput={(params) =>
+                                  <TextField onChange={handleInputChange} {...params}
+                                             label={'Enter your city ...'}/>}
+                              options={cities || []}/>
+
+                <Button disabled={geoLocation === undefined || isCurrentLocation === true} variant="contained"
+                        onClick={() => getGeoLocation()}><PositionSvg color={'#fff'}/></Button>
+            </div>
+        </>
+    )
 }
-
-
-  
-
-  return (
-    <>
-      <Form>
-   
-        <Form.Group className={styles.searchContainer}>
-          <Autocomplete
-            className={styles.searchInput}
-            clearOnBlur={false}
-            onChange={handleAutocompleteSelect}
-            getOptionLabel={(option) => option.formatted}
-            renderInput={(params) => (
-              <TextField
-                onChange={handleInputChange}
-                {...params}
-                label={"Enter your city ..."}
-              />
-            )}
-            options={cities || []}
-          />
-          <Button size="sm" variant="primary">
-            Search
-          </Button>
-        </Form.Group>
-      </Form>
-    </>
-  );
-};
