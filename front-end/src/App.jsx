9d95b1ffc2f19bb57b3171b9f90dfacf2656123a
Salma Elakhal <salma.elakhal.pro@gmail.com>
2025-04-02 02:27:49 +0200

    api

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,20 +1,24 @@
-import './App.module.scss';
-import React from 'react';
-import {SearchBar} from "./components/SearchBar/SearchBar";
-import {Wallpaper} from "./components/Wallpaper/Wallpaper";
-import {Weather} from "./components/Weather/Weather";
-import {Container} from "react-bootstrap";
+import "./App.module.scss";
+import React from "react";
+import { SearchBar } from "./components/SearchBar/SearchBar";
+import { Wallpaper } from "./components/Wallpaper/Wallpaper";
+import { Weather } from "./components/Weather/Weather";
+import { Container } from "react-bootstrap";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 function App() {
-    return (
-        <div className="App">
-            <Wallpaper/>
-            <Container>
-                <SearchBar/>
-                <Weather/>
-            </Container>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <Wallpaper />
+        <Container>
+          <SearchBar />
+          <Weather />
+        </Container>
+      </Provider>
+    </div>
+  );
 }
 
 export default App;
--- front-end/src/components/SearchBar/SearchBar.jsx
+++ front-end/src/components/SearchBar/SearchBar.jsx
@@ -1,14 +1,74 @@
-import {Button, Form} from "react-bootstrap";
-import styles from  './SearchBar.module.scss'
+import { Button, Form } from "react-bootstrap";
+import styles from "./SearchBar.module.scss";
+import { Autocomplete, TextField } from "@mui/material";
+import { useState } from "react";
+
 export const SearchBar = () => {
-    return (
-        <>
-            <Form>
-                <Form.Group className={styles.searchContainer}>
-                    <Form.Control className={styles.searchInput} size={'lg'} type='text' placeholder='Enter your city ...'></Form.Control>
-                    <Button size={'sm'} variant='primary'>Search</Button>
-                </Form.Group>
-            </Form>
-        </>
+  const [cities, setCities] = useState([]);
+  const [geoLocation, setGeoLocation] = useState(undefined);
+  const [isCurrentLocation, setIsCurrentLocation] = useState(false);
+
+  const GEO_API_KEY = process.env.REACT_APP_GEO_API_KEY;
+  const WEATHER_API_KEY = process.env.RACT_APP_WEATHER_API;
+
+  const handleInputChange = (e) => {
+    const { value } = e.currentTarget;
+    fetch(
+      `https://api.geoapify.com/v1/geocode/autocomplete?text=${value}&type=city&format=json&apiKey=${GEO_API_KEY}`
     )
-}
+      .then((response) => response.json())
+      .then((json) => setCities(json.results));
+  };
+
+  const handleAutocompleteSelect = (e, value) => {
+    if (value !== null) {
+      const { lon, lat } = value;
+
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+      )
+        .then((response) => response.json())
+        .then((json) =>
+          setCities(
+            json.results?.map((data) => {
+              const { lat, lon, city, country, formatted } = data;
+              return { lat, lon, city, country, formatted };
+            })
+          )
+        );
+
+      setIsCurrentLocation(false);
+      setGeoLocation({
+        lon,
+        lat,
+      });
+    } else {
+    }
+  };
+
+  return (
+    <>
+      <Form>
+        <Form.Group className={styles.searchContainer}>
+          <Autocomplete
+            className={styles.searchInput}
+            clearOnBlur={false}
+            onChange={handleAutocompleteSelect}
+            getOptionLabel={(option) => option.formatted}
+            renderInput={(params) => (
+              <TextField
+                onChange={handleInputChange}
+                {...params}
+                label={"Enter your city ..."}
+              />
+            )}
+            options={cities || []}
+          />
+          <Button size="sm" variant="primary">
+            Search
+          </Button>
+        </Form.Group>
+      </Form>
+    </>
+  );
+};
